79ae3c3eac31a67c0cd28171ea057e530082e53e
exebeeche <44062246+exebeeche@users.noreply.github.com>
2022-10-13 20:33:54 +0300

    Adding the lessons code samples

---
+++ c-sharp4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace c_sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple while cicle
+
+            //int counter = 0;
+            //int limit = int.Parse(Console.ReadLine());
+            //while (counter < limit)
+            //{
+            //    counter++;
+            //    Console.WriteLine(counter);
+            //}
+
+            // Odd and even numbers counter, also the sum of all odd and even numbers inside the specified range
+
+            uint oddNumberCounter = 0; // Odd number
+            uint evenNumberCounter = 0; // Even number
+
+            int oddNumberSum = 0; // Odd number summ
+            int evenNumberSum = 0; // Even number sum
+
+            Console.WriteLine("Enter the beginning of the number range");
+            int beginRange = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the end of the number range");
+            int endRange = int.Parse(Console.ReadLine());
+
+            while (beginRange <= endRange)
+            {
+                if (beginRange % 2 == 0)
+                {
+                    evenNumberCounter++;
+                    evenNumberSum += beginRange;
+                }
+                else
+                {
+                    oddNumberCounter++;
+                    oddNumberSum += beginRange;
+                }
+                beginRange++;
+            }
+
+
+            Console.WriteLine($"Even numbers: {evenNumberCounter}");
+            Console.WriteLine($"Odd numbers: {oddNumberCounter}");
+            Console.WriteLine($"Even numbers sum: {evenNumberSum}");
+            Console.WriteLine($"Odd numbers sum: {oddNumberSum}");
+
+        }
+    }
+}
